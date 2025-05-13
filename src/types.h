a76c6f8d7e8cfa53b0bbb66e388650bef6816d12
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-13 10:20:41 -0700

    include max/min

--- src/types.h
+++ src/types.h
@@ -2,6 +2,32 @@
 #define _TYPES_H__
 
 #define NULL ((void *)0)
+#define true ((_Bool)1)
+#define false ((_Bool)0)
+
+#define UINT8_MIN 0x0
+#define UINT16_MIN 0x0
+#define UINT32_MIN 0x0
+#define UINT64_MIN 0x0
+#define UINT128_MIN 0x0
+
+#define UINT8_MAX 0xFF
+#define UINT16_MAX 0xFFFF
+#define UINT32_MAX 0xFFFFFFFF
+#define UINT64_MAX 0xFFFFFFFFFFFFFFFF
+#define UINT128_MAX ((__uint128_t) - 1)
+
+#define INT8_MIN (-0x7F - 1)
+#define INT16_MIN (-0x7FFF - 1)
+#define INT32_MIN (-0x7FFFFFFF - 1)
+#define INT64_MIN (-0x7FFFFFFFFFFFFFFF - 1)
+#define INT128_MIN (-(((__int128_t)1 << 127) - 1) - 1)
+
+#define INT8_MAX 0x7F
+#define INT16_MAX 0x7FFF
+#define INT32_MAX 0x7FFFFFFF
+#define INT64_MAX 0x7FFFFFFFFFFFFFFF
+#define INT128_MAX (((__int128_t)1 << 127) - 1)
 
 typedef unsigned long size_t;
 typedef __int128_t int128_t;
@@ -16,7 +42,5 @@ typedef signed char int8_t;
 typedef unsigned char uint8_t;
 typedef unsigned char byte;
 typedef _Bool bool;
-#define true ((_Bool)1)
-#define false ((_Bool)0)
 
 #endif	// _TYPES_H__
